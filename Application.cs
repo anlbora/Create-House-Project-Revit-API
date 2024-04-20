db3c86470c1fb6c66c1b7f9e3b32fdf3a931faa5
Anil Bora <100442507+anlbora@users.noreply.github.com>
2024-04-20 18:26:32 +0300

    Add files via upload

---
+++ Application.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SimpleHouseProject
+{
+    /// <summary>
+    /// External application class to initialize and manage the Revit add-in.
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Application : IExternalApplication
+    {
+        /// <summary>
+        /// Called when Revit is shutting down.
+        /// </summary>
+        /// <param name="application">The controlled application.</param>
+        /// <returns>Result.Succeeded if successful.</returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Called when Revit starts up.
+        /// </summary>
+        /// <param name="application">The controlled application.</param>
+        /// <returns>Result.Succeeded if successful.</returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Create the ribbon panel and add the push button to it
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if (panel.AddItem(new PushButtonData("main", "Create House", thisAssemblyPath, "SimpleHouseProject.main"))
+                is PushButton button)
+            {
+                button.ToolTip = "Creating a house";
+            }
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Creates or retrieves the ribbon panel for the add-in.
+        /// </summary>
+        /// <param name="application">The controlled application.</param>
+        /// <returns>The ribbon panel for the add-in.</returns>
+        public RibbonPanel RibbonPanel(UIControlledApplication application)
+        {
+            string tab = "HOUSE";
+            RibbonPanel ribbonPanel = null;
+
+            // Attempt to create a new ribbon tab
+            try
+            {
+                application.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            // Attempt to create a new ribbon panel
+            try
+            {
+                application.CreateRibbonPanel(tab, "Create House");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            // Retrieve the ribbon panel
+            List<RibbonPanel> panelList = application.GetRibbonPanels(tab);
+            foreach (RibbonPanel panel in panelList.Where(p => p.Name == "Create House"))
+            {
+                ribbonPanel = panel;
+            }
+
+            return ribbonPanel;
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace SimpleHouseProject
+{
+    /// <summary>
+    /// Main external command class to execute the Revit add-in.
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class main : IExternalCommand
+    {
+        /// <summary>
+        /// Gets or sets the current Revit document.
+        /// </summary>
+        public Document doc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current Revit UI application.
+        /// </summary>
+        public UIApplication uiApp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current Revit UI document.
+        /// </summary>
+        public UIDocument uidoc { get; set; }
+
+        /// <summary>
+        /// Executes the main logic of the Revit add-in.
+        /// </summary>
+        /// <param name="commandData">The external command data.</param>
+        /// <param name="message">The message to return if the command fails.</param>
+        /// <param name="elements">The set of elements to which the command applies.</param>
+        /// <returns>Result.Succeeded if the command succeeds.</returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Initialize Revit API objects
+            uiApp = commandData.Application;
+            uidoc = uiApp.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            // Create and display the main UI form
+            MainUI mainPage = new MainUI(uidoc, doc);
+            mainPage.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
